920bfa7e7437b385c48f7fa7b0dfb46799aa888e
Elie Rotenberg <elie@rotenberg.io>
2014-11-30 20:31:13 +0100

    Upgraded all deps.

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require("6to5/polyfill");var Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var through = require("through2");
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var through = require("through2");
 var gutil = require("gulp-util");
 var _ref = require("react-nexus-style");
 
